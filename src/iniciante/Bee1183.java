483b7d44bae5cf5ce88a3f5490ca8c51a700b347
marcosguisleri <gustavoguisleri20@gmail.com>
2026-02-05 09:31:16 -0300

    feat: adicionar soluções dos exercícios Bee1182-1186 (Iniciante)

---
+++ src/iniciante/Bee1183.java
@@ -0,0 +1,41 @@
+package iniciante;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bee1183 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+
+        double[][] m = new double[12][12];
+
+        char tipo = input.next().charAt(0);
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                m[i][j] = input.nextDouble();
+            }
+        }
+
+        double soma = 0;
+        int contador = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if (j > i) {
+                    soma += m[i][j];
+                    contador++;
+                }
+            }
+        }
+
+        if (tipo == 'S') {
+            System.out.printf("%.1f\n", soma);
+        } else {
+            System.out.printf("%.1f\n", soma / contador);
+        }
+
+    }
+
+}
---
+++ src/iniciante/Bee1186.java
@@ -0,0 +1,41 @@
+package iniciante;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bee1186 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+
+        double[][] m = new double[12][12];
+
+        char tip = input.next().charAt(0);
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                m[i][j] = input.nextDouble();
+            }
+        }
+
+        double soma = 0;
+        int contador = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if (i + j > m.length - 1) {
+                    soma += m[i][j];
+                    contador++;
+                }
+            }
+        }
+
+        if (tip == 'S') {
+            System.out.printf("%.1f\n", soma);
+        } else {
+            System.out.printf("%.1f\n", soma / contador);
+        }
+
+    }
+
+}
